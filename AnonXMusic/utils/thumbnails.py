327585274280235dfa50d7b97604217c98248818
xd <85302664+okaybroxd@users.noreply.github.com>
2023-11-03 18:28:59 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -5,19 +5,19 @@ import aiofiles
 from config import YOUTUBE_IMG_URL
 
 async def gen_thumb(videoid):
-    if os.path.isfile(f"cache/Aviax{videoid}_v4.png"): 
+    if os.path.isfile(f"cache/AnonXMusic{videoid}_v4.png"): 
         return f"cache/Aviax{videoid}_v4.png"
 
-    url = f"https://aviaxapi.tech/thumb?videoid={videoid}"
+    url = f"https://AnonXMusicapi.tech/thumb?videoid={videoid}"
     
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(f"cache/Aviax{videoid}_4.png", mode="wb" ) 
+                    f = await aiofiles.open(f"cache/AnonXMusic{videoid}_4.png", mode="wb" ) 
                     await f.write(await resp.read()) 
                     await f.close()
 
-        return f"cache/Aviax{videoid}_4.png"
+        return f"cache/AnonXMusic{videoid}_4.png"
     except Exception:
         return YOUTUBE_IMG_URL
